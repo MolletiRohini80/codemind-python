aa5d4243bc9d222d95b8aa0ffb1ba6e6473fb479
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-18 22:15:09 +0530

    num=int(input()) square=num*num flag=0 while(num>0):     if(num%10!=square%10):         print("Not an Automorphic Number")         flag=1         break
        num=num//10                        
        square=square//10   
      
    if(flag==0):  
        print("Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,14 +1,14 @@
-n=int(input())
-r=0
-i=0
-t=n
-s=n*n
-while n!=0:
-    r=r+((s%10)*10**i);
-    s=s//10
-    n=n//10
-    i+=1
-if t==r:
+num=int(input())  
+square=num*num    
+flag=0   
+while(num>0):   
+    if(num%10!=square%10):  
+        print("Not an Automorphic Number")  
+        flag=1  
+        break                       
+       
+    num=num//10                        
+    square=square//10   
+  
+if(flag==0):  
     print("Automorphic Number")
-else:
-    print("Not an Automorphic Number")
